95152c5bec8051867419f40de85282e57990928f
Anshu Sain <sainanshu40@gmail.com>
2024-07-25 23:45:18 +0530

    coverImage path issue solved

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -44,10 +44,7 @@ const registerUser = asyncHandler( async(req,res,next)=>{
     
     const avatarLocalPath = req.files?.avatar?.[0]?.path
     // console.log(avatarLocalPath);
-    let coverImageLocalPath;
-    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
-        coverImageLocalPath = req.files.coverImage[0].path
-    }
+    const coverImageLocalPath = req.files?.coverImage?.[0]?.path
     // console.log(coverImageLocalPath);
 
     if(!avatarLocalPath){
