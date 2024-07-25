d28bf6d77712a410b39a39ca4437a11230917289
Anshu Sain <sainanshu40@gmail.com>
2024-07-25 23:34:00 +0530

    update

--- src/app.js
+++ src/app.js
@@ -4,12 +4,12 @@ import cookieParser from "cookie-parser"
 
 export const app = express();
 
-app.use(cors({
+app.use(cors({                         
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
 
-app.use(express.json({limit: "16kb"}))
+app.use(express.json({limit: "16kb"}))             // body-parser
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
