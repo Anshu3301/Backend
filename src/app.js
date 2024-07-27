87edd32ae04cac22f1de8f4c292dabc6b806b9f8
Anshu Sain <sainanshu40@gmail.com>
2024-07-28 00:53:25 +0530

    added auth.js

--- src/app.js
+++ src/app.js
@@ -9,10 +9,10 @@ app.use(cors({
     credentials: true
 }))
 
-app.use(express.json({limit: "16kb"}))             // body-parser
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.json({limit: "16kb"}))                          // body-parser to access JSON data
+app.use(express.urlencoded({extended: true, limit: "16kb"}))    // to access URL encoded data
 app.use(express.static("public"))
-app.use(cookieParser())
+app.use(cookieParser())                                         // to access Cookies in req & res
 
 
 // route import
